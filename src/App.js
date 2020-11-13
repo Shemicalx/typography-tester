414ac78554553fca22164226843190df9e9a9d10
Noam Shemi <noamshemi@gmail.com>
2020-11-13 14:45:48 +0200

    Initialize block-hover-menu

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React from "react";
 import './App.css';
-//Google Fonts API Link With Personal Key
-//https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyAgP9ZKkGmBu8yKCIRZXbyMKd1GNOibyWk
+const WebFont = require("webfontloader");
 
+/* FIGURE OUT * /
+WebFont.load({
+  google: {
+    families: ['Lobster']
+  }
+});
+/* */
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       text: "Lorem ipsum dolor sit amet, usu ea quis wisi. Pri ex justo soluta numquam, inani invidunt expetendis eu per. Te meis assueverit adversarium sea, ex illum blandit adolescens mel, ea mel graeco meliore scripserit. Nam fugit appareat ut, ad utamur senserit iudicabit vim, dico hendrerit ne vix.",
       fontProperties: {
+        /* To add new properties, add them here with a full range object -
+        propertyName: {
+          range: {
+            min: default minimum value in numbers,
+            max: default maximum value in numbers,
+            intervals: (max - min) / (number of grids - 1),
+            step: stepping distance for the number input,
+          }
+        }, */
         fontSize: {
           range: {
             min: 12,
-            max: 24,
-            // intervals = (max - min) / (number of grids - 1)
-            intervals: (24 - 12) / 5,
+            max: 17,
+            intervals: (17 - 12) / 5,
             step: 1,
           }
         },
@@ -23,7 +37,7 @@ class App extends React.Component {
             min: 1.2,
             max: 1.2,
             intervals: (1.2 - 1.2) / 5,
-            step: 0.05,
+            step: 0.1,
           }
         },
         letterSpacing: {
@@ -42,7 +56,8 @@ class App extends React.Component {
             step: 0.1,
           }
         },
-      }
+      },
+      showBlockMenu: [false,''],
     }
   }
 
@@ -68,64 +83,89 @@ class App extends React.Component {
     });
   };
 
-  handleDisplayBlockClick = (event) => {
-    //NEED TO RESET PROPERTIES TO MATCH CLICKED BLOCK
+  handleDisplayBlockClick = (styleProperties , event) => {
+    let fontPropertiesUpdate = this.state.fontProperties;
+    Object.entries(styleProperties).forEach(([propertyToUpdate, value]) => {
+      fontPropertiesUpdate[propertyToUpdate].range.min = Number(parseFloat(value).toFixed(2));
+      fontPropertiesUpdate[propertyToUpdate].range.max = Number(parseFloat(value).toFixed(2));
+      fontPropertiesUpdate[propertyToUpdate].range.intervals = 0;
+    });
+    this.setState(fontPropertiesUpdate);
   };
+
+  handleDisplayBlockHover = (blockNumber, show, event) => {
+    this.setState({showBlockMenu: [show, blockNumber]});
+    console.log(this.state.showBlockMenu);
+  }
 
   render() {
 
     return (
-      <div className="App">
+      <main className="App">
         <TestingSideBar 
           handleTextAreaChange={this.handleTextAreaChange}
           handleFontPropertyChange={this.handleFontPropertyChange}
           fontSizeRange={this.state.fontSizeRange}
           fontProperties={this.state.fontProperties}
         />
-        <main className="App__Display">
+        <div className="App__Display">
           <section className="App__DisplayGrid">
             {
               [...Array(6)].map( (_ , index) => {
+                //Function to calculate the value of each blocks property based on interval
+                const calculateBlockProperty = (property) => {
+                  const min = this.state.fontProperties[property].range.min;
+                  const intervalsToAdd = this.state.fontProperties[property].range.intervals * (index);
+                  return min + intervalsToAdd;
+                };
+                //Create a style object to implement each interval based value onto each block
+                const styleProperties = {};
+                Object.keys(this.state.fontProperties).forEach((property) => {
+                  styleProperties[property] = calculateBlockProperty(property)
+                });
+
                 return (
                   <DisplayBlock 
+                    blockNumber={index}
                     content={this.state.text} 
-                    fontSize={
-                      this.state.fontProperties.fontSize.range.min + this.state.fontProperties.fontSize.range.intervals * (index)
-                    }
-                    lineHeight={
-                      this.state.fontProperties.lineHeight.range.min + this.state.fontProperties.lineHeight.range.intervals * (index)
-                    }
-                    letterSpacing={
-                      this.state.fontProperties.letterSpacing.range.min + this.state.fontProperties.letterSpacing.range.intervals * (index)
-                    }
-                    wordSpacing={
-                      this.state.fontProperties.wordSpacing.range.min + this.state.fontProperties.wordSpacing.range.intervals * (index)
-                    }
+                    handleClick={this.handleDisplayBlockClick}
+                    handleHover={this.handleDisplayBlockHover}
+                    styleProperties={styleProperties}
+                    showBlockMenu={this.state.showBlockMenu}
                   />
                 )
               })
             }
           </section>
-        </main>
-      </div>
+        </div>
+      </main>
     );
   }
 }
 
 const DisplayBlock = (props) => {
-
   return (
   <div 
     className="DisplayBlock" 
-    //TEMPORARY 
-    style={{
-      'fontSize': `${props.fontSize}px`,
-      'lineHeight': `${props.lineHeight}`,
-      'letterSpacing': `${props.letterSpacing}px`,
-      'wordSpacing': `${props.wordSpacing}px`,
-    }}
+    onClick={(e)=>props.handleClick(props.styleProperties, e)}
+    onMouseEnter={(e)=>props.handleHover(props.blockNumber, true, e)}
+    onMouseLeave={(e)=>props.handleHover(props.blockNumber, false, e)}
   >
-    {props.content}
+    <div 
+      className="DisplayBlock__HoverMenu" 
+      style={ props.showBlockMenu[0] && props.blockNumber === props.showBlockMenu[1] ? {"left": "0%"} : {"left": "-50%"}}
+    >
+      <button>
+        Info
+      </button>
+      <button>
+        placeholder
+      </button>
+      <button>
+        Keep
+      </button>
+    </div>
+    <p style={props.styleProperties}>{props.content}</p>
   </div>
   )
 }
@@ -133,7 +173,7 @@ const DisplayBlock = (props) => {
 const TestingSideBar = (props) => {
 
   return (
-    <div className="SideBar">
+    <nav className="SideBar">
       <h1 className="SideBar__Title">
         Typography Grid Tester
       </h1>
@@ -144,6 +184,14 @@ const TestingSideBar = (props) => {
         placeholder="Write something!"
         onChange={props.handleTextAreaChange} 
       />
+      <div className="SideBar__MinMaxTitles">
+        <span>
+          Min
+        </span>
+        <span>
+          Max
+        </span>
+      </div>
       {
         Object.entries(props.fontProperties).map(([propertyName, propertyObj]) => {
           return (
@@ -155,7 +203,7 @@ const TestingSideBar = (props) => {
           )    
         })
       }
-    </div>
+    </nav>
   )
 }
 
@@ -166,10 +214,10 @@ const FontPropertyControls = (props) => {
   const {min, max, step} = props.range;
 
   return (
-    <div className={`SideBar__${propertyNameCapitalized}Range`}>
+    <div className={`SideBar__${propertyNameCapitalized}Range SideBar__RangeControls`}>
       <label className="SideBar__InputsLabel">
         {propertyNameCapitalized.replace(/([a-z])([A-Z])/g, '$1 $2')}
-        <span>{property === 'lineHeight' ? 'relative' : 'pixels' }</span>
+        <span>{property === 'lineHeight' ? ' relative' : ' pixels' }</span>
       </label>
       <div className="SideBar__Inputs">
         <input
